bee48729209083ff65a86aaa1ed2e22ae2f9b6f9
wwg135 <105048516+wwg135@users.noreply.github.com>
2022-05-28 15:12:34 +0800

    Update surgepro_reloadprofile.js

--- JS/Panel/surgepro_reloadprofile.js
+++ JS/Panel/surgepro_reloadprofile.js
@@ -34,11 +34,10 @@ if ($trigger == "button") {
 	$notification.post("配置重载","配置重载成功","")
 };
 $done({
-    title:"Surge👑Pro™ ",
     title:"«𝑺𝒖𝒓𝒈𝒆👑𝑷𝒓𝒐™» 🧚🏻‍♀️ ➺ ✌(՞ټ՞ )✌",
-    content: "北京时间："+ (new Date()).Format("yyyy-MM-dd HH:mm:ss")+"\n启动时长："+startTime + "\nMitm:"+icon_status(mitm_status.enabled)+"  Rewrite:"+icon_status(rewrite_status.enabled)+"  Scripting:"+icon_status(scripting_status.enabled),
-    icon: icon_s?"crown.fill":"exclamationmark.triangle",
-   "icon-color":icon_s?"#EACD76":"#F20C00"
+    content: "北京时间："+ (new Date()).Format("yyyy-MM-dd HH:mm:ss")+"\n启动时长："+startTime + "\n𝑴𝒊𝒕𝒎:"+icon_status(mitm_status.enabled)+"  𝑹𝒆𝒘𝒓𝒊𝒕𝒆:"+icon_status(rewrite_status.enabled)+"  𝑺𝒄𝒓𝒊𝒑𝒕𝒊𝒏𝒈:"+icon_status(scripting_status.enabled),
+    icon: icon_s?"crown.fill":"xmark.seal",
+   "icon-color":icon_s?"#e6b422":"#faff72"
 });
 })();
 function icon_status(status){
