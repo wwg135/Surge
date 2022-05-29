83747d8cedd88bc633aba049121589e6c971a112
wwg135 <105048516+wwg135@users.noreply.github.com>
2022-05-29 10:11:08 +0800

    Update sub_info.js

--- JS/Panel/sub_info.js
+++ JS/Panel/sub_info.js
@@ -9,7 +9,7 @@ Surge配置参考注释，
 示例↓↓↓ 
 ----------------------------------------
 [Script]
-Sub_info = type=generic,timeout=10,script-path= https://raw.githubusercontent.com/chaizia/Profiles/master/MySurge/sub_info_panel.js,script-update-interval=0,argument=url=[URL encode 后的机场节点链接]&reset_day=1&title=AmyInfo&icon=bonjour&color=#007aff
+Sub_info = type=generic,timeout=10,script-path= https://raw.githubusercontent.com/wwg135/Surge/main/JS/Panel/sub_info.js,script-update-interval=0,argument=url=[URL encode 后的机场节点链接]&reset_day=1&title=AmyInfo&icon=bonjour&color=#007aff
 
 [Panel]
 Sub_info = script-name=Sub_info,update-interval=86400
