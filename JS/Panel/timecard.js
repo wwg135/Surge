aad04c6ab0e73e396808b371f5224b5f5ad12f9a
wwg135 <105048516+wwg135@users.noreply.github.com>
2022-05-29 14:57:35 +0800

    Update timecard.js

--- JS/Panel/timecard.js
+++ JS/Panel/timecard.js
@@ -87,7 +87,7 @@ $done({
 title:title_random(tnumcount(Number(nowlist))),
 icon:icon_now(tnumcount(Number(nowlist))),
 content:tlist[nowlist][0]+":"+today(tnumcount(nowlist))+","+tlist[Number(nowlist) + Number(1)][0] +":"+ tnumcount(Number(nowlist) + Number(1))+ "天,"+tlist[Number(nowlist) + Number(2)][0]+":"+tnumcount(Number(nowlist) + Number(2))+"天"
-"icon-color":"#ff4500"
+"icon-color":"#FF4500"
 })
 
 function title_random(num){
