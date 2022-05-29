498a04d93625d2ec95b2a18a4354695e347624a8
wwg135 <105048516+wwg135@users.noreply.github.com>
2022-05-29 10:41:54 +0800

    Update sub_info.js

--- JS/Panel/sub_info.js
+++ JS/Panel/sub_info.js
@@ -1,64 +1,54 @@
 /*
-模板来自@mieqq大佬
-
-Surge配置参考注释
-
+Surge配置参考注释.
 示例↓↓↓ 
 ----------------------------------------
 [Script]
-Sub_info = type=generic,timeout=10,script-path= https://raw.githubusercontent.com/wwg135/Surge/main/JS/Panel/sub_info.js,script-update-interval=0,argument=url=[URL encode 后的机场节点链接]&reset_day=4&expire=2023-05-03&title=AmyInfo&icon=airplane.departure&color=#00ff00
-
+Sub_info = type=generic,timeout=10,script-path= https://raw.githubusercontent.com/wwg135/Surge/main/JS/Panel/sub_info_panel.js,script-update-interval=0,argument=url=[URL encode 后的机场节点链接]&reset_day=1&title=AmyInfo&icon=bonjour&color=#007aff
 [Panel]
-Sub_info = script-name=Sub_info,update-interval=43200
+Sub_info = script-name=Sub_info,update-interval=600
 ----------------------------------------
-
 先将带有流量信息的节点订阅链接encode，用encode后的链接替换"url="后面的[机场节点链接]
-
+（实在不会可以用这个捷径生成panel和脚本，https://www.icloud.com/shortcuts/3f24df391d594a73abd04ebdccd92584）
 可选参数 &reset_day，后面的数字替换成流量每月重置的日期，如1号就写1，8号就写8。如"&reset_day=8",不加该参数不显示流量重置信息。
-
-可选参数 &expire，机场链接不带expire信息的，可以手动传入expire参数，如"&expire=2022-02-01",注意一定要按照yyyy-MM-dd的格式。
-
+可选参数 &expire，机场链接不带expire信息的，可以手动传入expire参数，如"&expire=2022-02-01",注意一定要按照yyyy-MM-dd的格式。不希望显示到期信息也可以添加&expire=false取消显示。
 可选参数"title=xxx" 可以自定义标题。
-
 可选参数"icon=xxx" 可以自定义图标，内容为任意有效的 SF Symbol Name，如 bolt.horizontal.circle.fill，详细可以下载app https://apps.apple.com/cn/app/sf-symbols-browser/id1491161336
-
 可选参数"color=xxx" 当使用 icon 字段时，可传入 color 字段控制图标颜色，字段内容为颜色的 HEX 编码。如：color=#007aff
 ----------------------------------------
 */
 
+let args = getArgs();
+
 (async () => {
-  let args = getArgs();
   let info = await getDataInfo(args.url);
   if (!info) $done();
   let resetDayLeft = getRmainingDays(parseInt(args["reset_day"]));
-  
+
   let used = info.download + info.upload;
   let total = info.total;
   let expire = args.expire || info.expire;
-  let proportion = used / total;
-  let content = [`𝗨𝘀𝗲𝗱 : ${toPercent(proportion)} | 𝗔𝘃𝗹 : ${bytesToSize(total-used)}`];
-
-/*
+  
+  let content = [`使用:${toPercent(used)} | 总量:${bytesToSize(total)}`];
   if (resetDayLeft) {
-    content.push(`重置：剩余${resetDayLeft}天`);
+    content.push(`剩余:${toPercent(total-used)} | 重置:剩余${resetDayLeft}天`);
   }
-
-  if (expire) {
+  if (expire && expire !== "false") {
     if (/^[\d.]+$/.test(expire)) expire *= 1000;
     content.push(`到期：${formatTime(expire)}`);
   }
-*/
+ 
   let now = new Date();
   let hour = now.getHours();
   let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
   hour = hour > 9 ? hour : "0" + hour;
   minutes = minutes > 9 ? minutes : "0" + minutes;
-
+  seconds = seconds > 9 ? seconds : "0" + seconds;
   $done({
-    title: `${args.title} ` + `| 𝗥𝗲𝘀𝗲𝘁 : ` + `${resetDayLeft} Days`,
+    title: `${args.title} | ⏰：${hour}:${minutes}:${seconds}`,
     content: content.join("\n"),
     icon: args.icon || "airplane.departure",
-    "icon-color": args.color || "#00ff00",
+    "icon-color": args.color || "#ff00ff",
   });
 })();
 
@@ -72,9 +62,10 @@ function getArgs() {
 }
 
 function getUserInfo(url) {
+  let method = args.method || "head";
   let request = { headers: { "User-Agent": "Quantumult%20X" }, url };
   return new Promise((resolve, reject) =>
-    $httpClient.get(request, (err, resp) => {
+    $httpClient[method](request, (err, resp) => {
       if (err != null) {
         reject(err);
         return;
@@ -135,7 +126,6 @@ function bytesToSize(bytes) {
   let k = 1024;
   sizes = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
   let i = Math.floor(Math.log(bytes) / Math.log(k));
-  if (i === 4 ) return (((bytes / Math.pow(k, i)))*1024).toFixed(2)+ " " + sizes[i-1];
   return (bytes / Math.pow(k, i)).toFixed(2) + " " + sizes[i];
 }
 
@@ -145,9 +135,4 @@ function formatTime(time) {
   let month = dateObj.getMonth() + 1;
   let day = dateObj.getDate();
   return year + "年" + month + "月" + day + "日";
-}
-
-function toPercent(proportion) {
-  const percent = Number(proportion*100).toFixed(2);
-  return percent + "%";
 }
