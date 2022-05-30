f89d84973437c7763851511c5013bd51341ba39f
wwg135 <105048516+wwg135@users.noreply.github.com>
2022-05-30 12:27:48 +0800

    Update sub_info_panel.js

--- JS/Panel/sub_info_panel.js
+++ JS/Panel/sub_info_panel.js
@@ -55,7 +55,7 @@ let args = getArgs();
   minutes = minutes > 9 ? minutes : "0" + minutes;
 
   $done({
-    title: `${args.title} | ${hour}:${minutes}`,
+    title: `${args.title} | "⏰": ${hour}:${minutes}`,
     content: content.join("\n"),
     icon: args.icon || "airplane.circle",
     "icon-color": args.color || "#007aff",
